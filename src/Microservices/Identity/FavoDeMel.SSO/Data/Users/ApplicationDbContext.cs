d610f44eedf4a8264f1c51d3a818abe5082b0087
Marcio Queiroz <maqbr.dev@outlook.com>
2021-08-10 15:45:49 -0400

    Adiciona projeto SSO - IS4

---
+++ src/Microservices/Identity/FavoDeMel.SSO/Data/Users/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using FavoDeMel.SSO.Models;
+
+namespace FavoDeMel.SSO.Data.Users
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            // Customize the ASP.NET Identity model and override the defaults if needed.
+            // For example, you can rename the ASP.NET Identity table names and more.
+            // Add your customizations after calling base.OnModelCreating(builder);
+        }
+    }
+}
